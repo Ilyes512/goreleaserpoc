29668d9fbcf2b609d6d39db351b7827679b525bf
Ilyes512 <ilyes.ahidar@gmail.com>
2018-11-13 11:46:42 +0100

    Display received ldflags -X flags

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var (
+	version = "NOT_SET"
+	commit  = "NOT_SET"
+	date    = "NOT_SET"
+)
+
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println("Version: ", version)
+	fmt.Println("Commit: ", commit)
+	fmt.Println("Date: ", date)
 }
